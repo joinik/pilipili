ac624c1b0fac859b7cdfebd19c9910e55c59a298
joinik <1793844527@qq.com>
2025-01-16 19:11:14 +0800

    🔧从api12转到api13，平稳升级

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,2 +1,9 @@
+
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+// import { HapTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
+
 export { HapTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
+
+
+
+
